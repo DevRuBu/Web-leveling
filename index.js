96de257dbd803edd44f0ee6cd6f1aaaaff062409
devrubu <61195792+DevRuBu@users.noreply.github.com>
2020-04-02 17:33:08 +0300

    Commmit carent pack with work blog core

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
 const posts = [
     {
@@ -8,8 +12,8 @@ const posts = [
     text: 'Some text'
     },
     {
-        title: 'Title, Hi!',
-        text: 'Some text'
+        title: 'Page 2. Information technology (IT)',
+        text: 'Information technology (IT) is the use of computers to store, retrieve, transmit, and manipulate data[1] or information. IT is typically used within the context of business operations as opposed to personal or entertainment technologies.'
     },
     {
         title: 'Title, Hi!',
@@ -17,10 +21,41 @@ const posts = [
     }
 ];
 
-app.get('/posts', function(req,res){
+
+app.post('/blog', function(req, res) {
+    const data = req.body;
+    console.log(data);
+    posts.push(data);
     return res.send((posts));
+  });
+  
+
+  app.delete('/blog/:id', function(req, res) {
+    const id = req.params.id;
+    //const data = posts.splice([id]);
+    //console.log(data);
+    posts.delete([id]);
+    return res.send((posts));
+  });
+
+app.get('/blog', function(req,res){
+    return res.send((posts));
+});
+
+app.get('/blog/:id', function(req,res){
+    const id = req.params.id;
+    return res.send((posts[id]));
+});
+
+
+app.put('/blog/:id', express.json(), function(req,res){
+    const id = req.params.id;
+    posts[id].title = blogName;
+    posts[id].text = blogContent;
+    return res.send((posts[id]));
 });
 
 app.listen(3333, function() {
     console.log(`Server started on port`);
+    console.info('==> <img draggable="false" data-mce-resize="false" data-mce-placeholder="1" data-wp-emoji="1" class="emoji" alt="?" src="https://s.w.org/images/core/emoji/2.3/svg/1f30e.svg">  Go to http://localhost:%s', app.get('port'));
 });
