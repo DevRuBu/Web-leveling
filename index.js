f0292ac2780edae5cad3b130f76987b0ae3da10e
devrubu <61195792+DevRuBu@users.noreply.github.com>
2020-04-03 00:50:49 +0300

    Commmit carent version

--- index.js
+++ index.js
@@ -1,43 +1,87 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const path = require('path')
+const exphbs = require('express-handlebars')
+
 const app = express()
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/blog', (req, res) => {
+    res.render('indexH', {
+        title: firsPosts.title,
+        text: firsPosts.text
+    });
+})
+
+app.get('/blog/:id', function(req,res){
+    const id = req.params.id;
+    res.render('index', {
+        id: id,
+        title: posts[id].title,
+        text: posts[id].text
+    });
+});
+
+const firsPosts = {
+    title: 'Title, Hi!',
+    text: 'First page <br> <img src="https://www.enisa.europa.eu/news/enisa-news/privacy-standards-for-information-security/@@images/image" style="border-radius: 1em;opacity: 0.8; style="width: 303px; height: 150px;">'};
+
 const posts = [
     {
-    title: 'Title, Hi!',
-    text: 'Some text'
-    },
-    {
-        title: 'Page 2. Information technology (IT)',
+        title: 'Page 1. Information technology (IT)',
         text: 'Information technology (IT) is the use of computers to store, retrieve, transmit, and manipulate data[1] or information. IT is typically used within the context of business operations as opposed to personal or entertainment technologies.'
     },
     {
-        title: 'Title, Hi!',
+        title: 'Page 2. Information Security',
+        text: 'Information Security is basically the practice of preventing unauthorized access, use, disclosure, disruption, modification, inspection, recording or destruction of information. Information can be physical or electrical one.'
+    },
+    {
+        title: 'Page 3. Title, Hi!',
         text: 'Some text'
     }
 ];
 
 
-app.post('/blog', function(req, res) {
+app.post('/blog/:id', function(req, res) {
     const data = req.body;
     console.log(data);
     posts.push(data);
-    return res.send((posts));
+    return res.send(res.render('indexH', {
+        title: firsPosts.title,
+        text: firsPosts.text
+    })
+    );
   });
   
 
   app.delete('/blog/:id', function(req, res) {
     const id = req.params.id;
-    //const data = posts.splice([id]);
-    //console.log(data);
-    posts.delete([id]);
-    return res.send((posts));
+    const data = posts.splice([id]);
+    console.log(data);
+    return res.send((res.render('indexH', {
+        title: firsPosts.title,
+        text: firsPosts.text
+    })
+    ));
   });
 
+  
+app.get('/blogs', function(req,res){
+    return res.send((posts));
+});
+
+/*
 app.get('/blog', function(req,res){
     return res.send((posts));
 });
@@ -46,13 +90,18 @@ app.get('/blog/:id', function(req,res){
     const id = req.params.id;
     return res.send((posts[id]));
 });
-
+*/
 
 app.put('/blog/:id', express.json(), function(req,res){
     const id = req.params.id;
-    posts[id].title = blogName;
-    posts[id].text = blogContent;
-    return res.send((posts[id]));
+    const data = req.body;
+    posts[id].title = data.title;
+    posts[id].text = data.text;
+    return res.send((res.render('indexH', {
+        title: firsPosts.title,
+        text: firsPosts.text
+    })
+    ));
 });
 
 app.listen(3333, function() {
