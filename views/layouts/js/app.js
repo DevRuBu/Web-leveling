62d42b9cf541f966d179454cb5773cb56146ab90
devrubu <61195792+DevRuBu@users.noreply.github.com>
2020-04-17 12:03:26 +0300

    Commmit revrite version 0w.4t

--- views/layouts/js/app.js
+++ views/layouts/js/app.js
@@ -13,6 +13,7 @@ function CreatePage(title, text, id) {
         }
     }).then(response => {
     console.log(response);
+    window.location.reload();
     });
 }
 // Изменение 
@@ -30,27 +31,28 @@ function EditPage(title, text, id) {
         }
     }).then(response => {
     console.log(response);
+    window.location.reload();
     });
 }
 
 // del page
 function DeletePage(id) {
     let url ='/'+id;
-    //let tempUrl = 'http://localhost:3333/2';
     console.log(url);
-    fetch('http://localhost:3333/2', {
+    fetch(url, {
         method: 'DELETE'
     }).then(response => {
     console.log(response);
+    window.location.href = '/';
     });
  }
 
 function myFunctionSave() {
 	let titleInput = document.getElementsByName("title");
-	let textTexterea = document.getElementsByName("text");
     let idSpan = document.getElementsByName("id");
+	let textTexterea = document.getElementsByName("text");
 	let titles = titleInput[1].value;
-	let texts = textTexterea[1].value;
+	let texts = textTexterea[0].value;
     let ids = idSpan[0].innerText;
     EditPage(titles, texts, ids);
 }
@@ -58,14 +60,20 @@ function myFunctionAdd() {
 	let titleInput = document.getElementsByName("title");
 	let textTexterea = document.getElementsByName("text");
     let idSpan = document.getElementsByName("id");
+    console.log(textTexterea);
 	let titles = titleInput[1].value;
-	let texts = textTexterea[1].value;
+	let texts = textTexterea[0].value;
     let ids = idSpan[0].innerText;
     CreatePage(titles, texts, ids);
 }
 function myFunctionReset() {
-    let form = document.forms;
-    form.reset();
+    //window.location.reload();
+    let idSpan = document.getElementsByName("id");
+    let id = idSpan[0].innerText;
+    let url ='/'+id;
+    window.location.href = url;
+   /*let form = document.forms;
+    form.reset();*/
 }
 function myFunctionDelete() {
     let idSpan = document.getElementsByName("id");
